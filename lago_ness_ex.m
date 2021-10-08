ac440c5afa33d99df0387fa0b70002b2a0197ea3
Nico Quintero <juan24092002@gmail.com>
2021-10-08 11:00:04 -0500

    muchas cosas

--- lago_ness_ex.m
+++ lago_ness_ex.m
@@ -2,26 +2,29 @@ M = zeros(256, 256);
 for i=1:1:256
    M(:,i) = i; 
 end
+
+M = imread('lago_ness.pgm');
 figure(1);
 I = mat2gray(M);
 imshow(I);
 
 val_x = 1;
+val_y = 1;
 res = 80;
-im_size_x = 256;
-im_size_y = 256;
-salto_x = 2*val_x/im_size_x;
-salto_y = 2*pi/im_size_y;
+fil = size(M, 1)
+col = size(M, 2)
+salto_x = 2*val_x/col;
+salto_y = 2*val_y/fil;
 M_f = zeros(res, res);
 
-for i=1:1:im_size_x
-   for j=1:1:im_size_y
-       z = -val_x+(j*salto_x) + (pi-i*salto_y)*1i;
-       w = exp(z);
+for i=1:1:fil
+   for j=1:1:col
+       z = -val_x+(j*salto_x) + (val_y-i*salto_y)*1i;
+       w = sin(z);
        x_n = ceil((real(w) + 1.01)*(res/2));
-       y_n = ceil((imag(w) + 1.01)*(res/2));
+       y_n = ceil((1.01-imag(w))*(res/2));
        if 0 < x_n && x_n <= res && 0 < y_n && y_n <= res
-           M_f(x_n, y_n) = M(i,j);
+           M_f(y_n, x_n) = M(i,j);
        end
    end
 end
--- trans_ex.m
+++ trans_ex.m
@@ -7,19 +7,20 @@ I = mat2gray(M);
 imshow(I);
 
 val_x = 1;
+val_y = pi;
 res = 80;
 im_size_x = 256;
 im_size_y = 256;
 salto_x = 2*val_x/im_size_x;
-salto_y = 2*pi/im_size_y;
+salto_y = 2*val_y/im_size_y;
 M_f = zeros(res, res);
 
 for i=1:1:im_size_x
    for j=1:1:im_size_y
-       z = -val_x+(j*salto_x) + (pi-i*salto_y)*1i;
+       z = -val_x+(j*salto_x) + (val_y-i*salto_y)*1i;
        w = exp(z);
        x_n = ceil((real(w) + 1.01)*(res/2));
-       y_n = ceil((imag(w) + 1.01)*(res/2));
+       y_n = ceil((1.01-imag(w))*(res/2));
        if 0 < x_n && x_n <= res && 0 < y_n && y_n <= res
            M_f(x_n, y_n) = M(i,j);
        end
