dc6d11381c463bea96c5a25d8a20c31e45dd8725
Nico Quintero <juan24092002@gmail.com>
2021-10-15 09:21:56 -0500

    a

--- lago_ness_ex.m
+++ lago_ness_ex.m
@@ -8,8 +8,8 @@ figure(1);
 I = mat2gray(M);
 imshow(I);
 
-val_x = 1;
-val_y = 1;
+val_x = 2;
+val_y = pi;
 res = 80;
 fil = size(M, 1)
 col = size(M, 2)
@@ -20,7 +20,7 @@ M_f = zeros(res, res);
 for i=1:1:fil
    for j=1:1:col
        z = -val_x+(j*salto_x) + (val_y-i*salto_y)*1i;
-       w = sin(z);
+       w = sqrt(z);
        x_n = ceil((real(w) + 1.01)*(res/2));
        y_n = ceil((1.01-imag(w))*(res/2));
        if 0 < x_n && x_n <= res && 0 < y_n && y_n <= res
